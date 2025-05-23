4d2f756e01ac2a01a7ad42a7b768e5d4ab25c1ed
portbound <prtbnd@proton.me>
2025-05-22 21:24:09 -0400

    CH2

--- main.go
+++ main.go
@@ -1,18 +1,25 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
 
 func main() {
 	dir := http.Dir(".")
+	apiCfg := &apiConfig{}
 
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(dir)))
-	mux.HandleFunc("/healthz/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
-	})
+	mux.Handle("/app/", http.StripPrefix("/app/", apiCfg.middlewareMetricsInc(http.FileServer(dir))))
+	mux.HandleFunc("GET /api/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /api/reset", apiCfg.handlerReset)
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 
 	server := &http.Server{Addr: ":8080", Handler: mux}
 	server.ListenAndServe()
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
