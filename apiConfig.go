fbbe4312984774760af31fc2476c7301f119a7f5
portbound <prtbnd@proton.me>
2025-05-23 23:08:44 -0400

    CH4

--- apiConfig.go
+++ apiConfig.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 	"sync/atomic"
+	"unicode/utf8"
 )
 
 type apiConfig struct {
@@ -17,22 +20,84 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	})
 }
 
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(fmt.Sprintf(
-
 		`<html>
-  <body>
-    <h1>Welcome, Chirpy Admin</h1>
-		<p>Chirpy has been visited %d times!</p>
-  </body>
-</html>
-`, cfg.fileserverHits.Load())))
+			<body>
+				<h1>Welcome, Chirpy Admin</h1>
+				<p>Chirpy has been visited %d times!</p>
+			</body>
+		</html>
+		`, cfg.fileserverHits.Load())))
 }
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
 	cfg.fileserverHits.Store(0)
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Hits reset to 0"))
+}
+
+func handlerValidation(w http.ResponseWriter, r *http.Request) {
+	type requestBody struct {
+		Body string `json:"body"`
+	}
+	type responseBody struct {
+		Valid bool   `json:"valid"`
+		Body  string `json:"body"`
+		Error string `json:"error,omitempty"`
+	}
+
+	req := requestBody{}
+	res := responseBody{
+		Valid: true,
+		Body:  req.Body,
+		Error: "",
+	}
+	decoder := json.NewDecoder(r.Body)
+
+	code := http.StatusOK
+
+	err := decoder.Decode(&req)
+	if err != nil {
+		code = http.StatusBadRequest
+		res.Valid = false
+		res.Error = "Something went wrong"
+	}
+
+	if utf8.RuneCountInString(req.Body) > 140 {
+		code = http.StatusBadRequest
+		res.Valid = false
+		res.Error = "Chirp is too long"
+	}
+
+	res.Body = cleanChirp(req.Body)
+
+	dat, err := json.Marshal(res)
+	if err != nil {
+		code = http.StatusBadRequest
+		res.Valid = false
+		res.Error = "Something went wrong"
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write([]byte(dat))
+}
+
+func cleanChirp(s string) string {
+	s = strings.ToLower(s)
+
+	s = strings.ReplaceAll(s, "kerfuffle", "****")
+	s = strings.ReplaceAll(s, "sharbert", "****")
+	s = strings.ReplaceAll(s, "fornax", "****")
+
+	return s
 }
--- main.go
+++ main.go
@@ -12,14 +12,10 @@ func main() {
 	mux.Handle("/app/", http.StripPrefix("/app/", apiCfg.middlewareMetricsInc(http.FileServer(dir))))
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("POST /api/validate_chirp", handlerValidation)
 
 	server := &http.Server{Addr: ":8080", Handler: mux}
 	server.ListenAndServe()
-}
-
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
 }
